1975b0920d22aee482e729466728d9e292766b7d
Igor <153372339+naa091@users.noreply.github.com>
2025-07-27 12:44:05 +0300

    Did homework

--- NavigationApp/Modules/Feed/Controller/FeedViewController.swift
+++ NavigationApp/Modules/Feed/Controller/FeedViewController.swift
@@ -9,24 +9,37 @@ import UIKit
 import SnapKit
 
 final class FeedViewController: UIViewController {
-    private let model = FeedModel()
+    private let viewModel = FeedViewModel()
     private let feedView = FeedView()
-
+    
     override func loadView() {
-        let rootView = feedView
-        rootView.onOpenPostTapped = { [weak self] in self?.openPostTapped() }
-        rootView.onTestTapped = { [weak self] in self?.testTapped() }
-        rootView.onCheckTapped = { [weak self] in self?.checkGuess() }
-        self.view = rootView
+        feedView.onOpenPostTapped = { [weak self] in self?.openPostTapped() }
+        feedView.onTestTapped = { [weak self] in self?.testTapped() }
+        feedView.onCheckTapped = { [weak self] in self?.checkGuess() }
+        self.view = feedView
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        bindViewModel()
     }
 }
 
-// MARK: - Actions
+//MARK: - Private Extension
 private extension FeedViewController {
+    func bindViewModel() {
+        viewModel.onResultChanged = { [weak self] text, color in
+            DispatchQueue.main.async {
+                self?.feedView.resultLabel.text = text
+                self?.feedView.resultLabel.textColor = color
+            }
+        }
+    }
+
+    func checkGuess() {
+        viewModel.checkGuess(word: feedView.textField.text)
+    }
+
     func openPostTapped() {
         let postVC = PostViewController(customBackgroundColor: .systemOrange)
         navigationController?.pushViewController(postVC, animated: true)
@@ -35,19 +48,5 @@ private extension FeedViewController {
     func testTapped() {
         let postVC = PostViewController(customBackgroundColor: .systemMint)
         navigationController?.pushViewController(postVC, animated: true)
-    }
-
-    func checkGuess() {
-        let input = feedView.textField.text?.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
-        guard !input.isEmpty else {
-            feedView.resultLabel.text = "Input cannot be empty!"
-            feedView.resultLabel.textColor = .red
-            
-            return
-        }
-
-        let isCorrect = model.check(word: input)
-        feedView.resultLabel.text = isCorrect ? "Correct!" : "Incorrect!"
-        feedView.resultLabel.textColor = isCorrect ? .systemGreen : .systemRed
     }
 }
